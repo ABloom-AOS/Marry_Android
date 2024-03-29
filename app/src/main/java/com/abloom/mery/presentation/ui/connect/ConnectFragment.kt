d8a64c26d22f1d09002f2faafaa7906c50f1c524
ki960213 <ki960213@gmail.com>
2024-03-29 21:14:08 +0900

    [Feat/#51] 연결 화면에서 수행될 콜백과 방금 연결 성공 시 보여줄 화면 구현

--- app/src/main/java/com/abloom/mery/presentation/ui/connect/ConnectFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/connect/ConnectFragment.kt
@@ -7,6 +7,11 @@ import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentConnectBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
+import com.abloom.mery.presentation.common.util.copyToClipboard
+import com.abloom.mery.presentation.common.util.hideSoftKeyboard
+import com.abloom.mery.presentation.common.util.repeatOnStarted
+import com.abloom.mery.presentation.common.util.showToast
+import com.abloom.mery.presentation.common.view.InfoDialog
 import com.abloom.mery.presentation.common.view.setOnNavigationClick
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,15 +25,46 @@ class ConnectFragment : BaseFragment<FragmentConnectBinding>(R.layout.fragment_c
 
         setupAppBar()
         setupDataBinding()
+
+        observeConnectEvent()
     }
 
     private fun setupAppBar() {
-        binding.appbarConnect.setOnNavigationClick {
-            findNavController().popBackStack()
-        }
+        binding.appbarConnect.setOnNavigationClick { findNavController().popBackStack() }
     }
 
     private fun setupDataBinding() {
         binding.viewModel = viewModel
+        binding.onInvitationCodeCopyButtonClick = { invitationCode ->
+            copyToClipboard(invitationCode)
+            requireContext().showToast(R.string.connect_invitation_code_copy_success_message)
+        }
+        binding.onConnectButtonClick = { fianceInvitationCode ->
+            view?.hideSoftKeyboard()
+            viewModel.connectWithFiance(fianceInvitationCode)
+        }
+    }
+
+    private fun copyToClipboard(text: String) {
+        requireContext().copyToClipboard(
+            label = getString(R.string.connect_invitation_code_clipboard_label),
+            text = text
+        )
+    }
+
+    private fun observeConnectEvent() {
+        repeatOnStarted { viewModel.event.collect(::handleEvent) }
+    }
+
+    private fun handleEvent(event: ConnectEvent) {
+        when (event) {
+            ConnectEvent.ConnectFail -> {
+                InfoDialog(
+                    context = requireContext(),
+                    title = "연결에 실패했어요",
+                    message = "상대방의 연결 코드를 올바르게\n입력했는지 확인해주세요.",
+                ).show()
+            }
+        }
     }
 }
