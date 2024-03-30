9d1442ebaf8564852c64de79fe90afc768fa9c73
ki960213 <ki960213@gmail.com>
2024-03-31 00:11:31 +0900

    [Fix/#58] LoginDialogFragment에서 문자열을 companion object 대신 리소스에서 불러오도록 변경

--- app/src/main/java/com/abloom/mery/LoginDialogFragment.kt
+++ app/src/main/java/com/abloom/mery/LoginDialogFragment.kt
@@ -1,16 +1,14 @@
 package com.abloom.mery
 
-import android.annotation.SuppressLint
 import android.app.Dialog
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.abloom.domain.user.model.Authentication
 import com.abloom.mery.databinding.FragmentLoginDialogBinding
+import com.abloom.mery.presentation.common.util.showToast
 import com.abloom.mery.presentation.ui.home.HomeViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -49,11 +47,6 @@ class LoginDialogFragment : BottomSheetDialogFragment() {
         return BottomSheetDialog(requireContext(), R.style.AppBottomSheetDialogTheme)
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        init(context)
-    }
-
     private fun setupKakaoLoginButton() {
 
         binding.kakaoLoginBtn.setOnClickListener {
@@ -65,14 +58,14 @@ class LoginDialogFragment : BottomSheetDialogFragment() {
 
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null)
-                showToastMessage(KAKAO_LOGIN_FAILED)
+                context?.showToast(R.string.kakao_login_failed)
             else if (tokenInfo != null)
                 kakaoLoginSuccess()
         }
     }
 
     private fun kakaoLoginSuccess() {
-        showToastMessage(KAKAO_LOGIN_SUCCESS)
+        context?.showToast(R.string.kakao_login_text)
 
         // TODO("화면 이동 로직 구현")
         // 파이어베이스를 조회하여 기존 회원이 아닌 경우 회원가입 화면으로 이동한다.
@@ -88,39 +81,39 @@ class LoginDialogFragment : BottomSheetDialogFragment() {
                 when (error.toString()) {
 
                     AuthErrorCause.AccessDenied.toString() -> {
-                        showToastMessage(ACCESS_DENIED)
+                        context?.showToast(R.string.access_denied)
                     }
 
                     AuthErrorCause.InvalidClient.toString() -> {
-                        showToastMessage(INVALID_ERROR)
+                        context?.showToast(R.string.invalid_error)
                     }
 
                     AuthErrorCause.InvalidGrant.toString() -> {
-                        showToastMessage(CAN_NOT_AUTHENTICATION)
+                        context?.showToast(R.string.can_not_authentication)
                     }
 
                     AuthErrorCause.InvalidRequest.toString() -> {
-                        showToastMessage(REQUEST_PARAMETER_ERROR)
+                        context?.showToast(R.string.request_parameter_error)
                     }
 
                     AuthErrorCause.InvalidScope.toString() -> {
-                        showToastMessage(INVALID_SCOPE_ID)
+                        context?.showToast(R.string.invalid_scope_id)
                     }
 
                     AuthErrorCause.Misconfigured.toString() -> {
-                        showToastMessage(SETTING_NOT_RIGHT)
+                        context?.showToast(R.string.setting_not_right)
                     }
 
                     AuthErrorCause.ServerError.toString() -> {
-                        showToastMessage(SERVER_INTERNAL_ERROR)
+                        context?.showToast(R.string.server_internal_error)
                     }
 
                     AuthErrorCause.Unauthorized.toString() -> {
-                        showToastMessage(NOT_HAVE_REQUEST_PERMISSION)
+                        context?.showToast(R.string.not_have_request_permission)
                     }
 
                     else -> {
-                        showToastMessage(OTHER_ERROR)
+                        context?.showToast(R.string.other_error)
                     } // Unknown
 
                 }
@@ -134,31 +127,5 @@ class LoginDialogFragment : BottomSheetDialogFragment() {
             UserApiClient.instance.loginWithKakaoTalk(requireContext(), callback = callback)
         else
             UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
-    }
-
-    private fun showToastMessage(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
-    }
-
-    companion object {
-
-        @SuppressLint("StaticFieldLeak")
-        private lateinit var context: Context
-
-        fun init(context: Context) {
-            this.context = context
-        }
-
-        private val KAKAO_LOGIN_FAILED by lazy { context.getString(R.string.kakao_login_failed) }
-        private val KAKAO_LOGIN_SUCCESS by lazy { context.getString(R.string.kakao_login_text) }
-        private val OTHER_ERROR by lazy { context.getString(R.string.other_error) }
-        private val INVALID_ERROR by lazy { context.getString(R.string.invalid_error) }
-        private val ACCESS_DENIED by lazy { context.getString(R.string.access_denied) }
-        private val REQUEST_PARAMETER_ERROR by lazy { context.getString(R.string.request_parameter_error) }
-        private val CAN_NOT_AUTHENTICATION by lazy { context.getString(R.string.can_not_authentication) }
-        private val INVALID_SCOPE_ID by lazy { context.getString(R.string.invalid_scope_id) }
-        private val SETTING_NOT_RIGHT by lazy { context.getString(R.string.setting_not_right) }
-        private val SERVER_INTERNAL_ERROR by lazy { context.getString(R.string.server_internal_error) }
-        private val NOT_HAVE_REQUEST_PERMISSION by lazy { context.getString(R.string.not_have_request_permission) }
     }
 }
