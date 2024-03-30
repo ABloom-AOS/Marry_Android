4dcd8d0190e947e28f6b972d3b406f5c6378471a
ki960213 <ki960213@gmail.com>
2024-03-30 23:59:30 +0900

    [Style/#58] 코드 정렬

--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
@@ -1,6 +1,5 @@
 package com.abloom.mery.presentation.ui.category
 
-import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
