760e1b53e25ea51fe8ff18d38af19e869de9755e
ki960213 <ki960213@gmail.com>
2024-03-30 23:14:10 +0900

    [Feat/#51] HomeFragment에서 로그인 이벤트 처리

--- app/src/main/java/com/abloom/mery/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/home/HomeFragment.kt
@@ -2,17 +2,24 @@ package com.abloom.mery.presentation.ui.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.abloom.mery.LoginDialogFragment
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentHomeBinding
+import com.abloom.mery.presentation.MainViewModel
 import com.abloom.mery.presentation.common.base.BaseFragment
+import com.abloom.mery.presentation.common.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private val viewModel: HomeViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,6 +34,24 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         }
         binding.button11.setOnClickListener {
             findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToSignUpFragment())
+        }
+
+        observeMainEvent()
+    }
+
+    private fun observeMainEvent() {
+        repeatOnStarted {
+            mainViewModel.loginEvent
+                .combine(homeViewModel.isLogin) { _, isLogin -> isLogin }
+                .filterNotNull()
+                .collect(::handleLoginEvent)
+        }
+    }
+
+    private fun handleLoginEvent(isLogin: Boolean) {
+        if (!isLogin) {
+            val bottomSheetFragment = LoginDialogFragment()
+            bottomSheetFragment.show(childFragmentManager, LoginDialogFragment().tag)
         }
     }
 }
