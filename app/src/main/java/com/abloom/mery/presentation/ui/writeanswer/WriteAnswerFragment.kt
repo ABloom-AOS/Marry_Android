d7a1544e33fcad85f2daab75efee2e3048d73452
Choi YoonChan <choyc9239@gmail.com>
2024-03-30 17:01:02 +0900

    [Feat/#36] write_answer 뷰 구현 및 databinding color값 수정

--- app/src/main/java/com/abloom/mery/presentation/ui/writeanswer/WriteAnswerFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/writeanswer/WriteAnswerFragment.kt
@@ -1,29 +1,115 @@
 package com.abloom.mery.presentation.ui.writeanswer
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentWriteAnswerBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
+import com.abloom.mery.presentation.common.util.repeatOnStarted
+import com.abloom.mery.presentation.common.view.ConfirmDialog
 import com.abloom.mery.presentation.common.view.setOnActionClick
 import com.abloom.mery.presentation.common.view.setOnNavigationClick
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class WriteAnswerFragment :
     BaseFragment<FragmentWriteAnswerBinding>(R.layout.fragment_write_answer) {
 
-    private val viewModel: WriteAnswerViewModel by viewModels()
+    private val writeAnswerViewModel: WriteAnswerViewModel by viewModels()
+    private val args: WriteAnswerFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpAppBar()
+        setUpViewModel()
+        setUpQuestion()
+        observeAnswer()
+    }
+
+    private fun setUpAppBar() {
         binding.appbarWriteAnswer.setOnNavigationClick {
-            findNavController().popBackStack()
+            showPopBackDialog()
         }
+
         binding.appbarWriteAnswer.setOnActionClick {
-            findNavController().popBackStack(R.id.homeFragment, false)
+            showCompleteDialog()
         }
     }
+
+    private fun setUpViewModel() {
+        binding.viewModel = writeAnswerViewModel
+    }
+
+
+    private fun setUpQuestion(){
+        repeatOnStarted {
+            writeAnswerViewModel.question.collect{
+                binding.question = it?.content?: QUESTION_NULL
+            }
+        }
+    }
+
+    private fun observeAnswer() {
+        repeatOnStarted {
+            writeAnswerViewModel.answer.collect {
+                binding.textSize = it.length.toString()
+                when (it.length) {
+                    0 -> {
+                        binding.isEnabled = false
+                    }
+
+                    else -> {
+                        if (it.length <= TEXT_LIMIT_SIZE) {
+                            binding.isEnabled = true
+                            binding.tvTextSize.setTextColor(ContextCompat.getColor(requireContext(), R.color.neutral_40))
+                        } else {
+                            binding.isEnabled = false
+                            binding.tvTextSize.setTextColor(Color.parseColor(TEXT_RED_COLOR))
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun showPopBackDialog() {
+        ConfirmDialog(
+            context = requireContext(),
+            title = getString(R.string.writeanswer_popback_confirm_dialog_title),
+            message = getString(R.string.writeanswer_popback_confirm_dialog_message),
+            positiveButtonLabel = getString(R.string.writeanswer_complete),
+            onPositiveButtonClick = { findNavController().popBackStack(R.id.createQna, false) },
+            negativeButtonLabel = getString(R.string.all_cancel),
+        ).show()
+    }
+
+    private fun showCompleteDialog() {
+        ConfirmDialog(
+            context = requireContext(),
+            title = getString(R.string.writeanswer_complete_confirm_dialog_title),
+            message = getString(R.string.writeanswer_complete_confirm_dialog_message),
+            positiveButtonLabel = getString(R.string.writeanswer_complete),
+            onPositiveButtonClick = { findNavController().popBackStack(R.id.homeFragment, false) },
+            negativeButtonLabel = getString(R.string.all_cancel),
+        ).show()
+    }
+
+    companion object {
+
+        const val TEXT_LIMIT_SIZE = 150
+        const val TEXT_RED_COLOR = "#F00"
+        const val QUESTION_NULL = "질문 값이 없습니다."
+    }
 }
+
+
