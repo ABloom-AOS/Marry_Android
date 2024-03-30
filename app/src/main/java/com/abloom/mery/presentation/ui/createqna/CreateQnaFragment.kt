943f71539c98ec0347edaf17b8ea0dcb802d70bd
Choi YoonChan <choyc9239@gmail.com>
2024-03-30 23:20:24 +0900

    [Feat/#59] 램덤 로직 적용

--- app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
-import com.abloom.mery.createqnaretest.CreateQnaViewModel
 import com.abloom.mery.databinding.FragmentCreateQnaBinding
 import com.abloom.mery.presentation.common.base.BaseFragment
 import com.abloom.mery.presentation.common.view.setOnNavigationClick
@@ -15,8 +14,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragment_create_qna) {
 
-    //더미 데이터
-    private var id = 0L
     private val viewModel: CreateQnaViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,23 +21,14 @@ class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragme
         binding.appbarCreateQna.setOnNavigationClick {
             findNavController().popBackStack()
         }
-        viewModel.requestRandomQuestion()
-        viewModel.requestRandomId()
-        setupTodayQuestionText()
-        observeId()
+
+        setUpDataBinding()
         initListener()
     }
 
-    private fun setupTodayQuestionText() {
-        viewModel.randomQuestion.observe(viewLifecycleOwner) {
-            binding.tvTodayQuestion.text = it
-        }
-    }
-
-    private fun observeId() {
-        viewModel.id.observe(viewLifecycleOwner) {
-            id = it
-        }
+    private fun setUpDataBinding() {
+        binding.viewModel = viewModel
+        binding.navigateToWriteAnswerOnClick = ::navigateToWriteAnswer
     }
 
     private fun initListener() {
@@ -56,11 +44,13 @@ class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragme
         binding.ivFuture.setOnClickListener { goCategoryFragment(CategoryArgs.FUTURE) }
         binding.ivPresent.setOnClickListener { goCategoryFragment(CategoryArgs.PRESENT) }
         binding.ivPast.setOnClickListener { goCategoryFragment(CategoryArgs.PAST) }
+    }
 
-        binding.clQuestion.setOnClickListener {
-            val action = CreateQnaFragmentDirections.actionGlobalWriteAnswerFragment(id)
-            findNavController().navigate(action)
-        }
+    private fun navigateToWriteAnswer() {
+        val id = viewModel.todayRecommendationQuestion.value?.id ?: return
+        findNavController().navigate(
+            CreateQnaFragmentDirections.actionGlobalWriteAnswerFragment(id)
+        )
     }
 
     private fun goCategoryFragment(category: CategoryArgs) {
