0070e060f8abac99d41f56a8cbd017c03295096e
Choi YoonChan <choyc9239@gmail.com>
2024-03-31 20:22:29 +0900

    [Feat/#59] 오늘의 추천 로그인 및 비로그인 에 따른 버튼 로직 추가

--- app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaFragment.kt
@@ -1,24 +1,26 @@
 package com.abloom.mery.presentation.ui.createqna
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentCreateQnaBinding
+import com.abloom.mery.presentation.MainViewModel
 import com.abloom.mery.presentation.common.base.BaseFragment
 import com.abloom.mery.presentation.common.util.repeatOnStarted
 import com.abloom.mery.presentation.common.view.setOnNavigationClick
 import com.abloom.mery.presentation.ui.category.CategoryArgs
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filterNotNull
 
 @AndroidEntryPoint
 class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragment_create_qna) {
 
     private val viewModel: CreateQnaViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private var loginFlag = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,12 +28,32 @@ class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragme
             findNavController().popBackStack()
         }
         setUpDataBinding()
+        setupIsLogin()
+        setUpTodayQuestion()
         setUpListener()
     }
 
     private fun setUpDataBinding() {
         binding.viewModel = viewModel
-        binding.goWriteAnswerOnClick = ::navigateToWriteAnswer
+    }
+
+    private fun setUpTodayQuestion(){
+        repeatOnStarted {
+            viewModel.todayRecommendationQuestion.filterNotNull().collect{
+                binding.tvTodayQuestion.text = it.content
+            }
+        }
+    }
+
+    private fun setupIsLogin() {
+        repeatOnStarted {
+            viewModel.isLogin.filterNotNull().collect { isLogin ->
+                when (isLogin) {
+                    true -> { loginFlag = isLogin }
+                    false -> { loginFlag = isLogin }
+                }
+            }
+        }
     }
 
     private fun setUpListener() {
@@ -47,13 +69,22 @@ class CreateQnaFragment : BaseFragment<FragmentCreateQnaBinding>(R.layout.fragme
         binding.ivFuture.setOnClickListener { goCategoryFragment(CategoryArgs.FUTURE) }
         binding.ivPresent.setOnClickListener { goCategoryFragment(CategoryArgs.PRESENT) }
         binding.ivPast.setOnClickListener { goCategoryFragment(CategoryArgs.PAST) }
-    }
 
-    private fun navigateToWriteAnswer() {
-        val id = viewModel.todayRecommendationQuestion.value?.id ?: return
-        findNavController().navigate(
-            CreateQnaFragmentDirections.actionGlobalWriteAnswerFragment(id)
-        )
+        binding.clQuestion.setOnClickListener {
+            repeatOnStarted {
+                when(loginFlag){
+                    true -> {viewModel.todayRecommendationQuestion.filterNotNull().collect {
+                        findNavController().navigate(
+                            CreateQnaFragmentDirections.actionGlobalWriteAnswerFragment(it.id)
+                        )
+                    }}
+                    false ->{
+                        mainViewModel.dispatchLoginEvent()
+                        findNavController().popBackStack(R.id.homeFragment, false)
+                    }
+                }
+            }
+        }
     }
 
     private fun goCategoryFragment(category: CategoryArgs) {
