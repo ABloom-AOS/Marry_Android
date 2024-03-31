c4990c2df9856abc775c15bc447ab4a33ed5dd05
leeugun123 <leeugun123@naver.com>
2024-04-01 00:16:03 +0900

    [Feature/#50]: 체크박스에 따라 버튼 활성화 구현

--- app/src/main/java/com/abloom/mery/presentation/ui/signup/PrivacyConsentFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/signup/PrivacyConsentFragment.kt
@@ -2,6 +2,7 @@ package com.abloom.mery.presentation.ui.signup
 
 import android.os.Bundle
 import android.view.View
+import android.widget.CompoundButton
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.abloom.mery.R
@@ -17,74 +18,62 @@ class PrivacyConsentFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initBinding()
+        checkAgreeSignUpBtn()
+    }
 
-        binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
-
-            binding.checkBox1.isChecked = isChecked
-            binding.checkBox3.isChecked = isChecked
-            binding.checkBox4.isChecked = isChecked
-            binding.checkBox5.isChecked = isChecked
-
-            binding.agreeSignupBtn.isEnabled = isChecked
-
-            if (isChecked)
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
-            else
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
-        }
-
-        binding.checkBox1.setOnCheckedChangeListener { _, isChecked ->
-
-            binding.checkBox.isChecked = isChecked
-
-            binding.agreeSignupBtn.isEnabled = isChecked
-            if (isChecked)
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
-            else
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
-
-        }
-
-        binding.checkBox3.setOnCheckedChangeListener { _, isChecked ->
-
-            binding.checkBox.isChecked = isChecked
-
-            binding.agreeSignupBtn.isEnabled = isChecked
-            if (isChecked)
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
-            else
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
-
-        }
-
-        binding.checkBox4.setOnCheckedChangeListener { _, isChecked ->
-
-            binding.checkBox.isChecked = isChecked
-
-            binding.agreeSignupBtn.isEnabled = isChecked
-            if (isChecked)
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
-            else
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
-
-        }
-
-        binding.checkBox5.setOnCheckedChangeListener { _, isChecked ->
-
-            binding.checkBox.isChecked = isChecked
-
-            binding.agreeSignupBtn.isEnabled = isChecked
-
-            if (isChecked)
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
-            else
-                binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
-        }
-
+    private fun initBinding() {
+        binding.allConsentCheckBox.setOnClickListener { onCheckChanged(binding.allConsentCheckBox) }
+        binding.subCheckBox1.setOnClickListener { onCheckChanged(binding.subCheckBox1) }
+        binding.subCheckBox2.setOnClickListener { onCheckChanged(binding.subCheckBox2) }
+        binding.subCheckBox3.setOnClickListener { onCheckChanged(binding.subCheckBox3) }
+        binding.subCheckBox4.setOnClickListener { onCheckChanged(binding.subCheckBox4) }
+        binding.webTermsOfUseDetail.setOnClickListener {  }
+        binding.webSensitivePrivacyDetail.setOnClickListener {  }
+        binding.webPrivacyPolicyDetail.setOnClickListener {  }
         binding.agreeSignupBtn.setOnClickListener {
-            //viewModel.join()
             findNavController().popBackStack()
         }
+    }
 
+    private fun onCheckChanged(compoundButton: CompoundButton) {
+        if (compoundButton.id == R.id.all_consent_check_box)
+            setSubCheckBox(binding.allConsentCheckBox.isChecked)
+        else
+            binding.allConsentCheckBox.isChecked = checkAllSubCheckBox()
+
+        checkAgreeSignUpBtn()
+    }
+
+    private fun setSubCheckBox(checked: Boolean) {
+        binding.apply {
+            subCheckBox1.isChecked = checked
+            subCheckBox2.isChecked = checked
+            subCheckBox3.isChecked = checked
+            subCheckBox4.isChecked = checked
+        }
+    }
+
+    private fun checkAllSubCheckBox() =
+        binding.subCheckBox1.isChecked &&
+                binding.subCheckBox2.isChecked &&
+                binding.subCheckBox3.isChecked &&
+                binding.subCheckBox4.isChecked
+
+    private fun checkAgreeSignUpBtn() {
+        if (binding.allConsentCheckBox.isChecked)
+            activeAgreeSignUpBtn()
+        else
+            inActiveAgreeSignUpBtn()
+    }
+
+    private fun activeAgreeSignUpBtn() {
+        binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_selected_background)
+        binding.agreeSignupBtn.isEnabled = true
+    }
+
+    private fun inActiveAgreeSignUpBtn() {
+        binding.agreeSignupBtn.setBackgroundResource(R.drawable.agree_signup_btn_unselected_background)
+        binding.agreeSignupBtn.isEnabled = false
     }
 }
