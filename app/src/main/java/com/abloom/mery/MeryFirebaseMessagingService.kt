9da788df67b66f13bc2f0fdb3d03e1f358e9e3a9
leeugun123 <leeugun123@naver.com>
2024-04-03 23:10:21 +0900

    [Feat/#83]: notification 로고 및 기능 추가

--- app/src/main/java/com/abloom/mery/MeryFirebaseMessagingService.kt
+++ app/src/main/java/com/abloom/mery/MeryFirebaseMessagingService.kt
@@ -1,21 +1,80 @@
 package com.abloom.mery
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.abloom.mery.presentation.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MeryFirebaseMessagingService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {}
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            sendNotification(
+                remoteMessage.notification?.title,
+                remoteMessage.notification?.body
+            )
+        }
+    }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.e("IDService", "Refreshed token: $token")
+        Log.d("IDService", "Refreshed token: $token")
+
         sendRegistrationToServer(token)
     }
 
     private fun sendRegistrationToServer(token: String) {
         // 디바이스 토큰이 생성되거나 재생성 될 시 동작할 코드 작성
     }
+
+    private fun sendNotification(title: String?, body: String?) {
+
+        val notifyId = (System.currentTimeMillis() / 7).toInt()
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            action = Intent.ACTION_MAIN
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val pendingIntent =
+            PendingIntent.getActivity(
+                this, notifyId, intent,
+                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+            )
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(notifyId, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_mery"
+        private const val CHANNEL_NAME = "push"
+    }
+
 
 }
