f03da96cb2241ee92f76f19ed55a01644242154b
leeugun123 <leeugun123@naver.com>
2024-04-03 22:44:12 +0900

    [Feat/#83]: 알람 기능 체크

--- app/src/main/java/com/abloom/mery/presentation/MainActivity.kt
+++ app/src/main/java/com/abloom/mery/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.abloom.mery.presentation
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
@@ -20,11 +21,6 @@ import com.abloom.mery.R
 import com.abloom.mery.databinding.ActivityMainBinding
 import com.abloom.mery.presentation.common.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
-import android.Manifest
-import android.util.Log
-import com.abloom.mery.presentation.ui.qna.ResponseSelectDialog.Companion.TAG
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -55,29 +51,21 @@ class MainActivity : AppCompatActivity() {
         askNotificationPermission()
     }
 
-    // Declare the launcher at the top of your Activity/Fragment:
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
     ) { isGranted: Boolean ->
         if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
         } else {
-            // TODO: Inform user that that your app will not show notifications.
         }
     }
 
     private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
                 PackageManager.PERMISSION_GRANTED
             ) {
-                // FCM SDK (and your app) can post notifications.
             } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
             } else {
                 // Directly ask for the permission
                 requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
