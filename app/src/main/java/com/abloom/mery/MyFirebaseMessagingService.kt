0f6c6c65d249e52a1d20311fbec75b0a0b77e6b5
leeugun123 <leeugun123@naver.com>
2024-04-03 15:08:11 +0900

    [Feat/#83]: overrride fun onMessageReceived() 추가

--- app/src/main/java/com/abloom/mery/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/abloom/mery/MyFirebaseMessagingService.kt
@@ -2,9 +2,13 @@ package com.abloom.mery
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
