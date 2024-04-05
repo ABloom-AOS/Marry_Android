da11a4dc7c99d95437fb532b39bee6d39fafc0be
ki960213 <ki960213@gmail.com>
2024-04-06 05:29:07 +0900

    [Ref/#88] DataStore 객체를 의존성 주입하도록 변경

--- app/src/main/java/com/abloom/mery/data/datastore/MeryPreferencesDataSource.kt
+++ app/src/main/java/com/abloom/mery/data/datastore/MeryPreferencesDataSource.kt
@@ -1,39 +1,32 @@
 package com.abloom.mery.data.datastore
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class MeryPreferencesDataSource @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val dataStore: DataStore<Preferences>
 ) {
 
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "mery")
-
-    val loginUserId: Flow<String?> = context.dataStore.data
+    val loginUserId: Flow<String?> = dataStore.data
         .map { preferences ->
             val key = stringPreferencesKey(KEY_LOGIN_USER_ID)
             preferences[key]
         }
 
     suspend fun updateLoginUserId(userId: String) {
-        context.dataStore.edit { preferences ->
+        dataStore.edit { preferences ->
             val key = stringPreferencesKey(KEY_LOGIN_USER_ID)
             preferences[key] = userId
         }
     }
 
     suspend fun removeLoginUserId() {
-        context.dataStore.edit { preferences ->
+        dataStore.edit { preferences ->
             val key = stringPreferencesKey(KEY_LOGIN_USER_ID)
             preferences.remove(key)
         }
