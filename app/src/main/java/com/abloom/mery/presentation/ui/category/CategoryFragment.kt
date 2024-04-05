c6e6fc25802674409d4a217de6819e5421d3ffa0
ki960213 <ki960213@gmail.com>
2024-04-06 05:14:44 +0900

    [Fix/#88] category 화면에서 writeanswer화면으로 갔다가 다시 category 화면으로 갔을 때 상태가 유지되도록 수정

--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
@@ -1,240 +1,79 @@
 package com.abloom.mery.presentation.ui.category
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.abloom.domain.question.model.Category
-import com.abloom.domain.question.model.Question
 import com.abloom.mery.R
 import com.abloom.mery.databinding.FragmentCategoryBinding
 import com.abloom.mery.presentation.MainViewModel
-import com.abloom.mery.presentation.common.base.BaseFragment
+import com.abloom.mery.presentation.common.base.NavigationFragment
 import com.abloom.mery.presentation.common.util.repeatOnStarted
-import com.abloom.mery.presentation.common.view.setOnNavigationClick
+import com.abloom.mery.presentation.ui.category.recyclerview.QuestionAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class CategoryFragment : BaseFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
+class CategoryFragment : NavigationFragment<FragmentCategoryBinding>(R.layout.fragment_category) {
 
     private val mainViewModel: MainViewModel by activityViewModels()
-
-    private lateinit var categoryAdapter: CategoryAdapter
     private val categoryViewModel: CategoryViewModel by viewModels()
-    private val args: CategoryFragmentArgs by navArgs()
+
+    private val questionAdapter: QuestionAdapter by lazy { QuestionAdapter(::navigateToWriteAnswer) }
+
+    private fun navigateToWriteAnswer(questionId: Long) {
+        val action = CategoryFragmentDirections.actionGlobalWriteAnswerFragment(questionId)
+        findNavController().navigateSafely(action)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setupIsLogin()
-        setCategoryAdapter()
-        setupSelectedTabItem(args.category)
-        setupListener()
+        setupDataBinding()
+        setupQuestionRecyclerView()
+        setupCategoryTab()
+
+        observeCategory()
+        observeQuestions()
     }
 
-    private fun setupIsLogin() {
-
-        repeatOnStarted {
-            categoryViewModel.isLogin.filterNotNull().collect { isLogin ->
-                when (isLogin) {
-                    true -> {
-                        binding.clNologin.visibility = View.INVISIBLE
-                    }
-
-                    false -> {
-                        binding.clNologin.visibility = View.VISIBLE
-                    }
-                }
-
-            }
-        }
+    private fun setupDataBinding() {
+        binding.viewModel = categoryViewModel
+        binding.onUpButtonClick = { findNavController().popBackStack() }
+        binding.onLoginButtonClick = ::handleLoginButtonClick
     }
 
-    private fun setupSelectedTabItem(categoryArgs: CategoryArgs) {
-
-        when (categoryArgs) {
-            CategoryArgs.FINANCE ->
-                selectTabItem(
-                    Category.FINANCE,
-                    FINANCE_TABITEM_POSION
-                )
-
-            CategoryArgs.COMMUNICATION -> selectTabItem(
-                Category.COMMUNICATION,
-                COMMUNICATION_TABITEM_POSION
-            )
-
-            CategoryArgs.VALUES -> selectTabItem(
-                Category.VALUES,
-                VALUES_TABITEM_POSION
-            )
-
-            CategoryArgs.LIFESTYLE -> selectTabItem(
-                Category.LIFESTYLE,
-                LIFESTYLE_TABITEM_POSION
-            )
-
-            CategoryArgs.CHILDREN -> selectTabItem(
-                Category.CHILD,
-                CHILD_TABITEM_POSION
-            )
-
-            CategoryArgs.FAMILY -> selectTabItem(
-                Category.FAMILY,
-                FAMILY_TABITEM_POSION
-            )
-
-            CategoryArgs.SEX -> selectTabItem(
-                Category.SEX, SEX_TABITEM_POSION
-            )
-
-            CategoryArgs.HEALTH -> selectTabItem(
-                Category.HEALTH,
-                HEALTH_TABITEM_POSION
-            )
-
-            CategoryArgs.WEDDING -> selectTabItem(
-                Category.WEDDING,
-                WEDDING_TABITEM_POSION
-            )
-
-            CategoryArgs.FUTURE -> selectTabItem(
-                Category.FUTURE,
-                FUTURE_TABITEM_POSION
-            )
-
-            CategoryArgs.PRESENT -> selectTabItem(
-                Category.PRESENT,
-                PRESENT_TABITEM_POSION
-            )
-
-            CategoryArgs.PAST -> selectTabItem(
-                Category.PAST, PAST_TABITEM_POSION
-            )
-        }
+    private fun handleLoginButtonClick() {
+        mainViewModel.dispatchLoginEvent()
+        findNavController().popBackStack(R.id.homeFragment, false)
     }
 
-    private fun setupListener() {
+    private fun setupQuestionRecyclerView() {
+        binding.rv.adapter = questionAdapter
+    }
 
-        binding.appbarCategory.setOnNavigationClick {
-            findNavController().popBackStack()
-        }
-
-
-        binding.tvLoginTag.setOnClickListener {
-            mainViewModel.dispatchLoginEvent()
-            findNavController().popBackStack(R.id.homeFragment, false)
-        }
-
+    private fun setupCategoryTab() {
         binding.tb.onTabSelected { tab ->
-            repeatOnStarted {
-                categoryViewModel.questions.collect { map ->
-                    when (tab.position) {
-                        FINANCE_TABITEM_POSION -> {
-                            map[Category.FINANCE]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        COMMUNICATION_TABITEM_POSION -> {
-                            map[Category.COMMUNICATION]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        VALUES_TABITEM_POSION -> {
-                            map[Category.VALUES]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        LIFESTYLE_TABITEM_POSION -> {
-                            map[Category.LIFESTYLE]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        CHILD_TABITEM_POSION -> {
-                            map[Category.CHILD]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        FAMILY_TABITEM_POSION -> {
-                            map[Category.FAMILY]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        SEX_TABITEM_POSION -> {
-                            map[Category.SEX]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        HEALTH_TABITEM_POSION -> {
-                            map[Category.HEALTH]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        WEDDING_TABITEM_POSION -> {
-                            map[Category.WEDDING]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        FUTURE_TABITEM_POSION -> {
-                            map[Category.FUTURE]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        PRESENT_TABITEM_POSION -> {
-                            map[Category.PRESENT]?.let { categoryAdapter.setData(it) }
-                        }
-
-                        PAST_TABITEM_POSION -> {
-                            map[Category.PAST]?.let { categoryAdapter.setData(it) }
-                        }
-                    }
-                }
+            lifecycleScope.launch(Dispatchers.Main) {
+                binding.tb.setScrollPosition(tab.position, 0f, false)
             }
+            categoryViewModel.selectCategory(CategoryArgs.entries[tab.position])
         }
     }
 
-    private fun setCategoryAdapter() {
-        categoryAdapter = CategoryAdapter(::onCategoryItemClick)
-        val categoryManager =
-            LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
-        binding.rv.apply {
-            layoutManager = categoryManager
-            adapter = categoryAdapter
-            scrollToPosition(0)
-        }
-    }
-
-    private fun selectTabItem(category: Category, index: Int) {
-        binding.tb.selectTab(binding.tb.getTabAt(index))
-        lifecycleScope.launch(Dispatchers.Main) {
-            binding.tb.setScrollPosition(index, 0f, false)
-        }
+    private fun observeCategory() {
         repeatOnStarted {
-            categoryViewModel.questions.collect { map ->
-                map[category]?.let { questions ->
-                    categoryAdapter.setData(questions)
-                }
-
+            categoryViewModel.category.collect { category ->
+                binding.tb.selectTab(binding.tb.getTabAt(category.ordinal))
             }
         }
     }
 
-    private fun onCategoryItemClick(question: Question) {
-        val action =
-            CategoryFragmentDirections.actionGlobalWriteAnswerFragment(question.id)
-        findNavController().navigate(action)
-    }
-
-    companion object {
-
-        private const val FINANCE_TABITEM_POSION = 0
-        private const val COMMUNICATION_TABITEM_POSION = 1
-        private const val VALUES_TABITEM_POSION = 2
-        private const val LIFESTYLE_TABITEM_POSION = 3
-        private const val CHILD_TABITEM_POSION = 4
-        private const val FAMILY_TABITEM_POSION = 5
-        private const val SEX_TABITEM_POSION = 6
-        private const val HEALTH_TABITEM_POSION = 7
-        private const val WEDDING_TABITEM_POSION = 8
-        private const val FUTURE_TABITEM_POSION = 9
-        private const val PRESENT_TABITEM_POSION = 10
-        private const val PAST_TABITEM_POSION = 11
+    private fun observeQuestions() {
+        repeatOnStarted { categoryViewModel.questions.collect(questionAdapter::submitList) }
     }
 }
--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryViewModel.kt
@@ -1,35 +1,49 @@
 package com.abloom.mery.presentation.ui.category
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.abloom.domain.question.model.Category
 import com.abloom.domain.question.model.Question
 import com.abloom.domain.question.usecase.GetAvailableQuestionsUseCase
 import com.abloom.domain.user.usecase.GetLoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoryViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
     getLoginUserUseCase: GetLoginUserUseCase,
     getAvailableQuestionsUseCase: GetAvailableQuestionsUseCase
 ) : ViewModel() {
 
     val isLogin: StateFlow<Boolean?> = getLoginUserUseCase().map { it != null }
         .stateIn(
-            initialValue = null,
+            initialValue = true,
             started = SharingStarted.WhileSubscribed(5_000),
             scope = viewModelScope
         )
 
-    val questions: StateFlow<Map<Category, List<Question>>> = getAvailableQuestionsUseCase()
-        .stateIn(
-            initialValue = mapOf(),
+    val category: StateFlow<CategoryArgs> =
+        savedStateHandle.getStateFlow("category", CategoryArgs.FINANCE)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val questions: StateFlow<List<Question>> = category
+        .map(CategoryArgs::asDomain)
+        .flatMapLatest { category ->
+            getAvailableQuestionsUseCase().map { questions -> questions[category] ?: emptyList() }
+        }.stateIn(
+            initialValue = emptyList(),
             started = SharingStarted.WhileSubscribed(5_000),
             scope = viewModelScope
         )
+
+    fun selectCategory(categoryArgs: CategoryArgs) {
+        savedStateHandle["category"] = categoryArgs
+    }
 }
