aac9ca5c032ac51c88f6a893e28a5e30df93bd06
ki960213 <ki960213@gmail.com>
2024-04-06 12:22:15 +0900

    [Feat/#88] 오늘의 질문 로직 빼고 질문 저장소 구현

--- app/src/main/java/com/abloom/mery/data/repository/DefaultQuestionRepository.kt
+++ app/src/main/java/com/abloom/mery/data/repository/DefaultQuestionRepository.kt
@@ -2,21 +2,46 @@ package com.abloom.mery.data.repository
 
 import com.abloom.domain.question.model.Question
 import com.abloom.domain.question.repository.QuestionRepository
+import com.abloom.domain.user.repository.UserRepository
+import com.abloom.mery.data.database.RecommendationQuestionDao
+import com.abloom.mery.data.database.RecommendationQuestionEntity
 import com.abloom.mery.data.firebase.QuestionDocument
 import com.abloom.mery.data.firebase.QuestionFirebaseDataSource
 import com.abloom.mery.data.firebase.asExternal
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.flow.map
+import java.time.LocalDate
 import javax.inject.Inject
 
 class DefaultQuestionRepository @Inject constructor(
+    private val userRepository: UserRepository,
+    private val roomDataSource: RecommendationQuestionDao,
     private val firebaseDataSource: QuestionFirebaseDataSource
 ) : QuestionRepository {
 
-    override fun getTodayRecommendationQuestion(): Flow<Question> = getEssentialQuestions()
-        .map { it[0] }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun getTodayRecommendationQuestion(): Flow<Question> = userRepository.getLoginUser()
+        .flatMapLatest outer@{ loginUser ->
+            val loginUserId = loginUser?.id ?: RecommendationQuestionEntity.NOT_LOGIN_ID
+            roomDataSource.getRecommendationQuestionId(loginUserId, LocalDate.now())
+                .flatMapLatest { questionId ->
+                    if (questionId == null) return@flatMapLatest flow { }
+                    getQuestion(questionId)
+                }
+        }
 
     override suspend fun setTodayRecommendationQuestion(question: Question) {
+        val userId = userRepository.getLoginUser().lastOrNull()?.id
+        val entity = RecommendationQuestionEntity.create(
+            userId = userId,
+            date = LocalDate.now(),
+            questionId = question.id
+        )
+        roomDataSource.setRecommendationQuestion(entity)
     }
 
     override fun getEssentialQuestions(): Flow<List<Question>> = firebaseDataSource
--- app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaViewModel.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/createqna/CreateQnaViewModel.kt
@@ -6,11 +6,9 @@ import com.abloom.domain.question.model.Question
 import com.abloom.domain.question.usecase.GetTodayRecommendationQuestionUseCase
 import com.abloom.domain.user.usecase.GetLoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
