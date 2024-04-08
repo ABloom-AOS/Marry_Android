e23bca17a09ea0f12e4d230aed32643cfd55c292
ki960213 <ki960213@gmail.com>
2024-04-08 12:46:12 +0900

    [Fix/#93] 카테고리 화면에서 로그아웃 상태에서도 질문 목록이 잘 보여지도록 수정

--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryViewModel.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryViewModel.kt
@@ -1,17 +1,22 @@
 package com.abloom.mery.presentation.ui.category
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.abloom.domain.question.model.Category
 import com.abloom.domain.question.model.Question
 import com.abloom.domain.question.usecase.GetAvailableQuestionsUseCase
+import com.abloom.domain.question.usecase.GetQuestionsUseCase
 import com.abloom.domain.user.usecase.GetLoginUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -19,28 +24,44 @@ import javax.inject.Inject
 class CategoryViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     getLoginUserUseCase: GetLoginUserUseCase,
-    getAvailableQuestionsUseCase: GetAvailableQuestionsUseCase
+    getAvailableQuestionsUseCase: GetAvailableQuestionsUseCase,
+    getQuestionsUseCase: GetQuestionsUseCase
 ) : ViewModel() {
 
-    val isLogin: StateFlow<Boolean?> = getLoginUserUseCase().map { it != null }
+    val isLogin: StateFlow<Boolean> = getLoginUserUseCase().map { it != null }
         .stateIn(
             initialValue = true,
             started = SharingStarted.WhileSubscribed(5_000),
             scope = viewModelScope
         )
 
-    val category: StateFlow<CategoryArgs> =
-        savedStateHandle.getStateFlow("category", CategoryArgs.FINANCE)
+    val category: StateFlow<Category> = savedStateHandle
+        .getStateFlow("category", CategoryArgs.FINANCE)
+        .map(CategoryArgs::asDomain)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = Category.FINANCE
+        )
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val questions: StateFlow<List<Question>> = category
-        .map(CategoryArgs::asDomain)
-        .flatMapLatest { category ->
-            getAvailableQuestionsUseCase().map { questions -> questions[category] ?: emptyList() }
-        }.stateIn(
-            initialValue = emptyList(),
+    private val questions: StateFlow<Map<Category, List<Question>>> = isLogin
+        .flatMapLatest { isLogin ->
+            if (isLogin) getAvailableQuestionsUseCase() else getQuestionsUseCase()
+        }
+        .stateIn(
+            scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5_000),
-            scope = viewModelScope
+            initialValue = emptyMap()
+        )
+
+    val currentQuestions: StateFlow<List<Question>> =
+        combine(category, questions) { category, questions ->
+            questions[category] ?: emptyList()
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = emptyList()
         )
 
     fun selectCategory(categoryArgs: CategoryArgs) {
