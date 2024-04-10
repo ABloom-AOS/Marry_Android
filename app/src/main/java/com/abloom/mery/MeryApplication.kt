7068ab3ba983adc5fe1b23a38b77a48572a11fe1
Choi YoonChan <choyc9239@gmail.com>
2024-04-10 16:08:50 +0900

    [bug#103] 알람 삭제

--- app/src/main/java/com/abloom/mery/MeryApplication.kt
+++ app/src/main/java/com/abloom/mery/MeryApplication.kt
@@ -1,14 +1,8 @@
 package com.abloom.mery
 
-import android.app.AlarmManager
 import android.app.Application
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
-import java.util.Calendar
-import java.util.TimeZone
 
 @HiltAndroidApp
 class MeryApplication : Application() {
@@ -16,31 +10,5 @@ class MeryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
-        scheduleDailyNotification()
-    }
-
-    private fun scheduleDailyNotification() {
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(this, AlarmReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val calendar = Calendar.getInstance().apply {
-            timeZone = TimeZone.getTimeZone("Asia/Seoul")
-            set(Calendar.HOUR_OF_DAY, 9)
-            set(Calendar.MINUTE, 0)
-            set(Calendar.SECOND, 0)
-        }
-
-        alarmManager.setRepeating(
-            AlarmManager.RTC_WAKEUP,
-            calendar.timeInMillis,
-            AlarmManager.INTERVAL_DAY,
-            pendingIntent
-        )
     }
 }
