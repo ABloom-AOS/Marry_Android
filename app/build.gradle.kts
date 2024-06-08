0dd2ff3bbcbbd72f45222290337b73e00e8dafdc
leeugun123 <leeugun123@naver.com>
2024-06-08 22:32:07 +0900

    [Chore/#127]: 버전 코드 및 버전명 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.abloom.mery"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.1"
+        versionCode = 6
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
