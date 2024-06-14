0d9daff4bec9d38165a0358c652571681156e7de
leeugun123 <leeugun123@naver.com>
2024-06-14 19:21:54 +0900

    [Fix/#129]: 팝업창 뜨기 메소드명 수정

--- app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/abloom/mery/presentation/ui/category/CategoryFragment.kt
@@ -50,7 +50,7 @@ class CategoryFragment : NavigationFragment<FragmentCategoryBinding>(R.layout.fr
         observeCategory()
         observeQuestions()
 
-        showPopUpDialogWithDelay()
+        showPopUpDialogIfNeed()
     }
 
     private fun setupDataBinding() {
@@ -105,7 +105,7 @@ class CategoryFragment : NavigationFragment<FragmentCategoryBinding>(R.layout.fr
         repeatOnStarted { categoryViewModel.currentQuestions.collect(questionAdapter::submitList) }
     }
 
-    private fun showPopUpDialogWithDelay() {
+    private fun showPopUpDialogIfNeed() {
         lifecycleScope.launch {
             delay(DIALOG_DISPLAY_DELAY_TIME)
             if (checkPopUpDialogCondition()) {
