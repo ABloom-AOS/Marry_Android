612597646555d6137d408128ac1c9965f027d737
ki960213 <ki960213@gmail.com>
2024-06-18 21:56:28 +0900

    [Feat/#134] AnnouncementDocument에 createdAt 프로퍼티 추가

--- app/src/main/java/com/abloom/mery/data/firebase/announcements/AnnouncementDocument.kt
+++ app/src/main/java/com/abloom/mery/data/firebase/announcements/AnnouncementDocument.kt
@@ -2,16 +2,20 @@ package com.abloom.mery.data.firebase.announcements
 
 import com.abloom.domain.announcement.model.Announcement
 import com.abloom.mery.data.firebase.Document
+import com.abloom.mery.data.firebase.toLocalDateTime
+import dev.gitlive.firebase.firestore.Timestamp
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AnnouncementDocument(
     val title: String = "",
     val url: String = "",
+    val createdAt: Timestamp = Timestamp.now(),
 ) : Document {
 
     fun asExternal() = Announcement(
         title = title,
         url = url,
+        createdAt = createdAt.toLocalDateTime()
     )
 }
--- domain/src/main/java/com/abloom/domain/announcement/model/Announcement.kt
+++ domain/src/main/java/com/abloom/domain/announcement/model/Announcement.kt
@@ -1,6 +1,9 @@
 package com.abloom.domain.announcement.model
 
+import java.time.LocalDateTime
+
 data class Announcement(
     val title: String,
     val url: String,
+    val createdAt: LocalDateTime,
 )
